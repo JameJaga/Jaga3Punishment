0e79cceb4fefc1a517890c31c1f7441496f95707
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-29 17:21:03 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,10 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
-async def on_message(message):            
+async def on_message(message):     
+    if message.content == '/jphelp':
+        embed = discord.Embed(title="Jaga3PunishmentHelp",description="/jpmute {user}で{user}を発言禁止\n/jpkick　{user}で{user}をキック\n/jpban {user}で{user}をBAN",color=0xff0000)
+        embed.add_field(name="perdon",value="/jpunmute {user}で{user}の発言禁止解除 /jpperdon {user}で{user}のBAN解除。")
+        await channel.send(embed=embed)
     
 client.run(TOKEN)
