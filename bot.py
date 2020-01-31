7d4008a71dc1d8bd8b57328c6b0965e8bbcf0a7a
JameJaga <59597475+JameJaga@users.noreply.github.com>
2020-01-31 15:39:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    channel = client.get_channel(663570894521761846)
     guild = message.guild
     if message.author.bot:
         return
@@ -20,12 +21,14 @@ async def on_message(message):
         embed = discord.Embed(title="Jaga3PunishmentHelp",description="/jpmute {user}で{user}を発言禁止\n/jpkick　{user}で{user}をキック\n/jpban {user}で{user}をBAN",color=discord.Colour.blue())
         embed.add_field(name="**Perdon**",value="/jpunmute {user}で{user}の発言禁止解除\n/jpperdon {user}で{user}のBAN解除。")
         await message.channel.send(embed=embed)
+        await channel.send(embed=embed)
     #Kickコマンド
     elif message.content.startswith('/jpkick'):
         member = message.mentions[0]
         await guild.kick(user=member)
         embed = discord.Embed(title="MemberKicked",description = f'{str(member)}をキックしました。',color=discord.Colour.from_rgb(255, 0, 0))
-        await message.channel.send(embed=embed)   
+        await message.channel.send(embed=embed)
+        await channel.send(embed=embed)
     #muteコマンド
     elif message.content.startswith('/jpmute'):
         member = message.mentions[0]
@@ -33,18 +36,21 @@ async def on_message(message):
         await member.add_roles(role)
         embed = discord.Embed(title="MemberMuted",description = f'{str(member)}をミュートしました。',color=discord.Colour.from_rgb(255, 0, 0))
         await message.channel.send(embed=embed)
+        await channel.send(embed=embed)
     #unmuteコマンド
-    elif message.content.startswith('/jpremute'):
+    elif message.content.startswith('/jpunmute'):
         member = message.mentions[0]
         role = discord.utils.get(message.guild.roles, name='Muted')
         await member.remove_roles(role)
         embed = discord.Embed(title="MemberUnmuted",description = f'{str(member)}のミュートを解除しました。',color=discord.Colour.from_rgb(255, 0, 247))
         await message.channel.send(embed=embed)
+        await channel.send(embed=embed)
     #banコマンド
     elif message.content.startswith('/jpban'):
         member = message.mentions[0]
         await guild.ban(user=member)
         embed = discord.Embed(title="MemberBanned",description = f'{str(member)}をBANしました。',color=discord.Colour.from_rgb(255, 0, 0))
         await message.channel.send(embed=embed)
+        await channel.send(embed=embed)
         
 client.run(TOKEN)
